3812987e1db8dd4c1824606200003f6223098059
Zarra <zarrabeitia272@gmail.com>
2021-12-14 12:52:02 +0100

    julen zarrabeitia

--- DiccionarioInformatico/src/base/Diccionario.java
+++ DiccionarioInformatico/src/base/Diccionario.java
@@ -3,21 +3,23 @@ package base;
 import java.util.Scanner;
 
 public class Diccionario {
-	
+
 	private static Scanner teclado = new Scanner(System.in);
-	
+
 	public Diccionario() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		String palabra = new String();
-		System.out.print ("Introduce palabra: ");
+		System.out.print("Introduce palabra: ");
 		palabra = teclado.next();
-		
-		if(palabra != null && palabra.equalsIgnoreCase("bit")) {
+
+		if (palabra != null && palabra.equalsIgnoreCase("bit")) {
 			System.out.println("Mínima unidad de información que representa un 0 ó un 1.");
-		}else {
+		} else if (palabra != null && palabra.equalsIgnoreCase("manzana")) {
+			System.out.println("Fruta de color verde que sale del manzano.");
+		} else {
 			System.out.println("Palabra no encontrada");
 		}
 
