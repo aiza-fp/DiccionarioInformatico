316f1e161aa2fcf5bec30c1883deb6d87dc82bf5
Ander Caballero <a_caballero@fpzornotza.com>
2021-12-14 12:46:37 +0100

    Diccionario Ander

--- DiccionarioInformatico/src/base/Diccionario.java
+++ DiccionarioInformatico/src/base/Diccionario.java
@@ -17,7 +17,9 @@ public class Diccionario {
 		
 		if(palabra != null && palabra.equalsIgnoreCase("bit")) {
 			System.out.println("Mínima unidad de información que representa un 0 ó un 1.");
-		}else {
+		} else if (palabra != null && palabra.equalsIgnoreCase("Videojuego")) {
+			System.out.println("Un videojuego es un juego electrónico en el que una o más personas interactúan por medio de un controlador, con un dispositivo que muestra imágenes de vídeo.");
+		} else {
 			System.out.println("Palabra no encontrada");
 		}
 
