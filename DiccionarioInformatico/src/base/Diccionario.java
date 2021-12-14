42800807950ddc844f64aaa5b872995be6db8feb
Javier Saez <j_saez@fpzornotza.com>
2021-12-14 12:45:47 +0100

    Modificacion del diccionario

--- DiccionarioInformatico/src/base/Diccionario.java
+++ DiccionarioInformatico/src/base/Diccionario.java
@@ -12,15 +12,23 @@ public class Diccionario {
 
 	public static void main(String[] args) {
 		String palabra = new String();
+		do {
 		System.out.print ("Introduce palabra: ");
 		palabra = teclado.next();
 		
-		if(palabra != null && palabra.equalsIgnoreCase("bit")) {
-			System.out.println("Mínima unidad de información que representa un 0 ó un 1.");
-		}else {
-			System.out.println("Palabra no encontrada");
+		if(palabra != null && palabra.equalsIgnoreCase("palabra")) {
+			System.out.println("Unidad léxica constituida por un sonido o conjunto de sonidos articulados que tienen un significado fijo y una categoría gramatical.");
 		}
+		else {
+			System.out.println("Significado:");
+			System.out.println("Palabra no encontrada, introduce otra palabra");}
+		}
+		
+		while(!(palabra != null && palabra.equalsIgnoreCase("palabra")));{
 
-	}
+		}
+		}
+		
+
 
 }
