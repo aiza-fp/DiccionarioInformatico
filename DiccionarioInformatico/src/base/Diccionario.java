81574f9aaf735086949f08b056d234580aaeaea3
IkerMusatadi <i_musatadi@fpzornotza.com>
2021-12-17 10:45:46 +0100

    Cambios Iker Diccionario

--- DiccionarioInformatico/src/base/Diccionario.java
+++ DiccionarioInformatico/src/base/Diccionario.java
@@ -17,11 +17,7 @@ public class Diccionario {
 		
 		if(palabra != null && palabra.equalsIgnoreCase("bit")) {
 			System.out.println("Mínima unidad de información que representa un 0 ó un 1.");
-		}else {
-			System.out.println("Palabra no encontrada");
-		}
-		
-		if (palabra != null && palabra.equalsIgnoreCase("gigabyte")) { //Iker Musatadi
+		}else if (palabra != null && palabra.equalsIgnoreCase("gigabyte")) { //Iker Musatadi
 			System.out.println("Es una unidad de almacenamiento de información cuyo símbolo es el GB");
 		}else {
 			System.out.println("Palabra no encontrada");
