0a007e8ccfc415721193878286aaa050b668fdcd
Jon <Ikaslea@DESKTOP-0QDQJI1>
2022-11-18 14:42:30 +0100

    Jon Losada

--- DiccionarioInformatico/src/base/Diccionario.java
+++ DiccionarioInformatico/src/base/Diccionario.java
@@ -73,7 +73,8 @@ public class Diccionario {
 			System.out.println("Es una secuencia de instrucciones de código que se ejecuta repetidas veces.");
 		}else if (palabra != null && palabra.equalsIgnoreCase("GPU")) {
 			System.out.println("Una unidad de procesamiento gráfico es un coprocesador dedicado al procesamiento de gráficos u operaciones, para aligerar la carga de trabajo del procesador/CPU central en aplicaciones como los videojuegos o aplicaciones 3D interactivas");
-		}
+		} else if (palabra != null && palabra.equalsIgnoreCase("Criptomoneda")) {
+			System.out.println("Una criptomoneda es un medio digital de intercambio que utiliza criptografía fuerte para asegurar las transacciones, controlar la creación de unidades adicionales y verificar la transferencia de activos usando tecnologías de registro distribuido.");
 
 		else {
 			System.out.println("Palabra no encontrada");
